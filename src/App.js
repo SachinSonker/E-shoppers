ee75047f01cfc92463ec5c34bfca5cbd25ea6b64
Chirag Sahuji <chirag_sahuji@persistent.com>
2023-05-25 14:21:25 +0530

    header/footer

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Box, Typography,Container, styled } from '@mui/material'
+
+const Typo = styled(Typography)({
+  color: "#646464",
+  margin: "5px 14px 0",
+  fontFamily: 'Mulish',
+  fontStyle: 'normal',
+  fontWeight: 400,
+  fontSize: '14px',
+  lineHeight: '21px'
+})
+const Footer = () => {
+
+  return (
+    <Box sx={{display:"flex",justifyContent:"center",position:"fixed",width:"100%",backgroundColor:"#F7F7F7",height:"35px",bottom:0 }}>
+          <Typo>FAQs</Typo>
+          <Typo>Orders & Return</Typo>
+          <Typo>Account</Typo>
+          <Typo>About Us</Typo>
+    </Box>
+  )
+}
+
+export default Footer

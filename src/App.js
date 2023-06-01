afad8afd3ce5ab31f92a81c43158562445f663fc
vedantPersi <vedant_khandokar@persistent.com>
2023-06-01 18:44:00 +0530

    Navbar and Footer set

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import ProductDetails from './Components/ProductDetailPage/ProductDetail';
-import LandingPage from './Components/LandingPage/LandingPage';
+import Landing from './Components/LandingPageComponent/Landing';
 import CheckoutPage from './Components/Checkout/CheckoutPage';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 
 function App() {
@@ -13,9 +14,27 @@ function App() {
     <GoogleOAuthProvider clientId="307607756850-v5ohbkpfepkgfrd2duss39nd5cse9gta.apps.googleusercontent.com">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path="productdetails/*" element={<ProductDetails />} />
-          <Route path="checkout/*" element={<CheckoutPage />} />
+          <Route path='/' element={
+            <div className='App'>
+              <Navbar/>
+              <Landing/>
+              <Footer/>
+            </div>
+          } />
+          <Route path="productdetails/*" element={
+            <div className='App'>
+              <Navbar/>
+              <ProductDetails/>
+              <Footer/>
+            </div>
+          } />
+          <Route path="checkout/*" element={
+            <div className='App'>
+              <Navbar/>
+              <CheckoutPage/>
+              <Footer/>
+            </div>
+          } />
         </Routes>
       </BrowserRouter>
     </GoogleOAuthProvider>

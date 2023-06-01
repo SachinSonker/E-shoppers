398d1fe23d5687ce4aaacfc6b812fea2e31b87c1
vedantPersi <vedant_khandokar@persistent.com>
2023-06-01 14:59:58 +0530

    Removed Checkout from landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Landing from '../LandingPageComponent/Landing' ;
 import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
-import CheckoutPage from '../Checkout/CheckoutPage';
 
 function LandingPage() {
   return (
@@ -10,7 +9,6 @@ function LandingPage() {
       <div className='App'>
         <Navbar />
         <Landing />
-        <CheckoutPage />
       </div>
       <Footer />
     </React.Fragment>

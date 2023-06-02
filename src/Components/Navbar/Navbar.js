bddc9d52fb2173d7548d4f98caf16618fb656d98
Chirag Sahuji <chirag_sahuji@persistent.com>
2023-06-02 15:26:17 +0530

    adding no of items funtionality in cart

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,32 +4,17 @@ import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import Login from '../Login/Login';
 import Signup from '../Signup/Signup';
-// import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 import './Navbar.css';
 import Cart from '../Cart/Cart';
 
 
-const noOfItems = 0;
 
 const Search = styled("div")({
     marginRight: "11px",
     width: "600px",
 })
-// const Shop = styled(Button)({
-//     textTransform: "none",
-//     color: "ButtonText",
-//     '&:hover,&:active': {
-//         textDecoration:'underline',
-//         backgroundColor:'transparent'
-//     },
-//     fontSize: "15px",
-//     margin: "0 10px 0",
-//     fontFamily: 'Mulish',
-//     fontStyle: "normal",
-//     fontWeight: "400",
-//     lineHeight: "5px"
-// })
+
 const SignIn = styled(Button)({
     textTransform: "none",
     border: "1px solid #A0A0A0",
@@ -54,6 +39,7 @@ const Navbar = () => {
     const [placement, setPlacement] = useState();
     const [open, setOpen] = useState(false);
     const [anchorEl, setAnchorEl] = useState(null);
+    const [noOfItem, setItem] = useState(3);
 
     const openModal= ()=>{
       setModalOpen(true)
@@ -65,6 +51,10 @@ const Navbar = () => {
     function createAccount(){
         setRegistration(true)
         setModalOpen(false)
+    }
+    function itemDelete() {
+        const no = noOfItem - 1
+        setItem(no)
     }
 
     function openSignIn(){
@@ -122,7 +112,7 @@ const Navbar = () => {
                     <Box>
                         
                         <Button sx={{ color:'white',width:'110px', margin: "0 10px 0", boxSizing: "border-box", backgroundColor:'#8B3DFF','&:hover': {backgroundColor:'#7300e6'},height:'39px'}} variant="contained" endIcon={<ShoppingBagIcon />} onClick={handleClick('bottom')}>
-                                <Typography sx={{marginTop:'5px',textTransform: "none", fontSize: "15px", fontFamily: 'Mulish', fontStyle: 'normal', fontWeight: 400, lineHeight: '27px'}}>{noOfItems} Items</Typography>
+                            <Typography sx={{ marginTop: '5px', textTransform: "none", fontSize: "15px", fontFamily: 'Mulish', fontStyle: 'normal', fontWeight: 400, lineHeight: '27px' }}>{noOfItem} Items</Typography>
                         </Button>  
                             
                             <Popper
@@ -132,11 +122,11 @@ const Navbar = () => {
                                 placement={placement}
                                 onClose={handleClose}
                                 sx={{ zIndex : 1500, width:'344px', height:'609px'}}>
-                                <Cart onClose={handleClose} />
+                            <Cart onClose={handleClose} itemRemove={ itemDelete} />
 
                             </Popper>
                         { !signIn ?  <SignIn variant='contained' onClick={openModal}>Sign In</SignIn>: ""}
-                        { signIn ? <AccountCircleRoundedIcon /> : ""}
+                        {signIn ? <AccountCircleRoundedIcon sx={{color:'#8B3DFF'}} /> : ""}
                         {isOpen ?  <Login onClose={closeModal} registration={createAccount} loggedIn={login}/>:""}
                         {registration ?  <Signup onClose={()=> setRegistration(false)} signin={openSignIn} loggedIn={login} />:""}
                     </Box>
